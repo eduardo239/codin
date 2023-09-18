2f056906055c3f2b861c35ac083f5c182f39e267
eduardo239 <eucrieiumaconta@gmail.com>
2023-09-18 16:59:14 -0300

    first commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quas
+      quam error officiis delectus? Tempora, voluptas laboriosam! Ut ipsam quasi
+      minima error, voluptatum doloremque temporibus vitae corporis impedit
+      placeat consectetur?
+    </>
+  );
+}
+
+export default App;
